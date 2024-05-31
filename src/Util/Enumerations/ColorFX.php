f1de4f91209ba13b21b38713797b7ffb5acc1dcb
Andrew Masiye <amasiye313@gmail.com>
2024-05-31 16:41:34 +0200

    feat(util): create color helper enumerations

--- src/Util/Enumerations/ColorFX.php
+++ src/Util/Enumerations/ColorFX.php
@@ -2,7 +2,15 @@
 
 namespace Assegai\Console\Util\Enumerations;
 
-class ColorFX
+/**
+ * Enumerates the text styles.
+ *
+ * @package Assegai\Console\Util\Enumerations
+ */
+enum ColorFX: string
 {
-
+  case BLINK = "\033[5m";
+  case BOLD = "\033[1m";
+  case UNDERLINE = "\033[4m";
+  case REVERSE = "\033[7m";
 }
