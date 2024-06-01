158fc24e3d7e34839339d8e418d3a2f369bace42
Andrew Masiye <amasiye313@gmail.com>
2024-06-01 12:33:21 +0200

    feat(commands): create DumpAutoload.php command

--- src/Commands/DumpAutoload.php
+++ src/Commands/DumpAutoload.php
@@ -2,9 +2,28 @@
 
 namespace Assegai\Console\Commands;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+  name: 'dump:autoload',
+  description: 'Dumps the autoloader',
+  aliases: ['da'])
+]
 class DumpAutoload extends Command
 {
+  public function execute(InputInterface $input, OutputInterface $output): int
+  {
+    $dumpAutoloadResult = passthru('composer dump-autoload --ansi');
 
+    if (false === $dumpAutoloadResult)
+    {
+      $output->writeln('<error>Failed to dump the autoloader</error>');
+      return Command::FAILURE;
+    }
+
+    return Command::SUCCESS;
+  }
 }
