921ee306213b108bd2c9e7647fd436987bbe6553
Andrew Masiye <amasiye313@gmail.com>
2024-06-02 09:18:11 +0200

    feat(commands): update Commands
    * Updated DumpAutoload command. Renamed command from dump:autoload to dump-autolaod
    * Updated NewProject command. Updated execute method

--- src/Commands/DumpAutoload.php
+++ src/Commands/DumpAutoload.php
@@ -2,13 +2,14 @@
 
 namespace Assegai\Console\Commands;
 
+use Assegai\Console\Util\Inspector;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-  name: 'dump:autoload',
+  name: 'dump-autoload',
   description: 'Dumps the autoloader',
   aliases: ['da'])
 ]
@@ -16,6 +17,14 @@ class DumpAutoload extends Command
 {
   public function execute(InputInterface $input, OutputInterface $output): int
   {
+    $inspector = new Inspector($input, $output);
+
+    if ($inspector->isValidWorkspace(getcwd()) === false)
+    {
+      $output->writeln('<error>Not a valid workspace</error>');
+      return Command::FAILURE;
+    }
+
     $dumpAutoloadResult = passthru('composer dump-autoload --ansi');
 
     if (false === $dumpAutoloadResult)
--- src/Commands/NewProject.php
+++ src/Commands/NewProject.php
@@ -2,7 +2,42 @@
 
 namespace Assegai\Console\Commands;
 
-class NewProject
-{
+use Assegai\Console\Core\WorkspaceManager;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Helper\ProgressIndicator;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+  name: 'new',
+  description: 'Creates a new project',
+  aliases: ['n']
+)]
+class NewProject extends Command
+{
+  public function configure(): void
+  {
+    $this->addArgument('name', InputArgument::OPTIONAL, 'The name of the project');
+    $this->addOption('directory', 'd', InputArgument::OPTIONAL, 'The path to create the project in', getcwd());
+  }
+
+  public function execute(InputInterface $input, OutputInterface $output): int
+  {
+    $projectName = $input->getArgument('name');
+    $projectDirectory = $input->getOption('directory');
+
+    /** @var FormatterHelper $formatter */
+    $formatter = $this->getHelper('formatter');
+    /** @var QuestionHelper $questionHelper */
+    $questionHelper = $this->getHelper('question');
+    $workspaceManager = new WorkspaceManager($input, $output, $formatter, $questionHelper);
+
+    $workspaceManager->init($projectName, $projectDirectory);
+
+    return Command::SUCCESS;
+  }
 }
