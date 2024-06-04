fde82d57a91fd1aed97f3a7b9005faedf57a47e8
Andrew Masiye <amasiye313@gmail.com>
2024-06-04 10:02:32 +0200

    feat(core): create EnumSchematic.php

---
+++ src/Core/Schematics/EnumSchematic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Assegai\Console\Core\Schematics;
+
+use Assegai\Console\Core\Schematics\Enumerations\ClassTemplate;
+
+/**
+ * EnumSchematic class. This class is used to generate Enum classes.
+ *
+ * @package Assegai\Console\Core\Schematics
+ */
+class EnumSchematic extends AbstractClassSchematic
+{
+  public function configure(): void
+  {
+    $this->template = ClassTemplate::ENUM;
+  }
+}
