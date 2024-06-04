480a292f583f074c593a64129854a1fead1d21ab
Andrew Masiye <amasiye313@gmail.com>
2024-06-04 10:03:25 +0200

    feat(core): create PipeSchematic.php

---
+++ src/Core/Schematics/PipeSchematic.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Assegai\Console\Core\Schematics;
+
+/**
+ * A pipe schematic. This class is used to generate Pipe classes.
+ *
+ * @package Assegai\Console\Core\Schematics
+ */
+class PipeSchematic extends AbstractClassSchematic
+{
+  public function configure(): void
+  {
+    $this->suffix = 'pipe';
+    $this->imports = [
+      'Assegai\Core\Interfaces\IPipeTransform',
+      'Assegai\Core\Attributes\Injectable',
+      'stdClass'
+    ];
+    $this->attributes = ['Injectable'];
+    $this->interfaces = ['IPipeTransform'];
+    $this->regex[] = [
+      'pattern' => '/{\s+public/',
+      'replacement' => "{\n  public",
+    ];
+    $this->methods = [
+      <<<PHP
+  public function transform(mixed \$value, array|stdClass|null \$metaData = null): mixed
+  {
+    // TODO: implement transform()
+
+    return \$value;
+  }
+PHP
+    ];
+  }
+}
