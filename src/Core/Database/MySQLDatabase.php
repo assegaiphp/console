63cc12defee4c6c8c78db96398840f53baa21e11
Andrew Masiye <amasiye313@gmail.com>
2024-06-12 19:53:40 +0200

    fix(core/database): resolve issue when setting up mysql database
    * Fixed MySQLDatabase.php

--- src/Core/Database/MySQLDatabase.php
+++ src/Core/Database/MySQLDatabase.php
@@ -12,8 +12,11 @@ use Assegai\Console\Util\Path;
 use Exception;
 use PDO;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * Class MySQLDatabase. This class is a MySQL database connection.
@@ -26,6 +29,9 @@ class MySQLDatabase extends PDO implements DatabaseConnectionInterface
    * @var Inspector
    */
   protected Inspector $inspector;
+
+  const int ERROR_UNKNOWN_DATABASE = 1049;
+  const int ERROR_INVALID_CREDENTIALS = 1045;
 
   /**
    * MySQLDatabase constructor.
@@ -48,17 +54,33 @@ class MySQLDatabase extends PDO implements DatabaseConnectionInterface
       exit(Command::FAILURE);
     }
 
-    $dbConfig = new DBConfig($this->input, $this->output, $this->name, DatabaseType::MYSQL->value);
+    $type = DatabaseType::MYSQL->value;
+    $dbConfig = new DBConfig($this->input, $this->output, $this->name, $type);
     $dbConfig->load();
+    $configPath = "$type.$this->name";
 
-    $host = $dbConfig->get('host') ?? DEFAULT_MYSQL_HOST;
-    $username = $dbConfig->get('username') ?? '';
-    $password = $dbConfig->get('password') ?? '';
-    $port = $dbConfig->get('port') ?? DEFAULT_MYSQL_PORT;
+    $host = $dbConfig->get("$configPath.host") ?? DEFAULT_MYSQL_HOST;
+    $username = $dbConfig->get("$configPath.username") ?? $dbConfig->get("$configPath.user") ?? '';
+    $password = $dbConfig->get("$configPath.password") ?? '';
+    $port = $dbConfig->get("$configPath.port") ?? DEFAULT_MYSQL_PORT;
 
     $dsn = "mysql:host=$host;port=$port;dbname=$this->name";
 
-    parent::__construct($dsn, $username, $password);
+    try
+    {
+      parent::__construct($dsn, $username, $password);
+    }
+    catch (Exception $exception)
+    {
+      $message = match((int)$exception->getCode()) {
+        self::ERROR_UNKNOWN_DATABASE => 'Database not found',
+        self::ERROR_INVALID_CREDENTIALS => 'Invalid credentials',
+        default => $exception->getMessage()
+      };
+
+      $this->output->writeln("<error>({$exception->getCode()}): $message</error>");
+      exit(Command::FAILURE);
+    }
   }
 
   /**
@@ -67,35 +89,65 @@ class MySQLDatabase extends PDO implements DatabaseConnectionInterface
   public static function exists(string $name): bool
   {
     $input = new MockInput();
-    $output = new MockOutput();
+    $output = new ConsoleOutput();
+    $helper = new QuestionHelper();
+    $type = DatabaseType::MYSQL->value;
 
     try
     {
       $inspector = new Inspector($input, $output);
       $workingDirectory = getcwd() ?: '';
-      $configFilename = Path::join($workingDirectory, 'config', 'local.php');
 
-      if (! $inspector->isValidWorkspace($workingDirectory))
+      if (! $inspector->isValidWorkspace($workingDirectory) )
       {
         $output->writeln('<error>This is not a valid workspace.</error>');
         return false;
       }
 
-      if (! file_exists($configFilename))
+      $dbConfig = new DBConfig($input, $output, $name, $type);
+      $dbConfig->load();
+      $configPath = "$type.$name";
+
+      if ( is_null($dbConfig->get($configPath)) )
       {
-        $output->writeln('<error>Database configuration does not exist.</error>');
+        $output->writeln([
+          "<error>Database config for $name not found.</error>",
+          "\n<comment>Run `assegai database:configure $name` to configure the database.</comment>"
+        ]);
+        exit(Command::FAILURE);
+      }
+
+      $errorOutputPath = Path::join($workingDirectory, time() . '.error.log');
+      $user = $dbConfig->get("$configPath.username") ?? $dbConfig->get("$configPath.user", DEFAULT_MYSQL_USER);
+      $password = $dbConfig->get("$configPath.password") ?? '';
+      $host = $dbConfig->get("$configPath.host", DEFAULT_MYSQL_HOST);
+      $port = $dbConfig->get("$configPath.port", DEFAULT_MYSQL_PORT);
+
+      $result = @`mysql -u $user -p$password -h $host -P $port -e "SHOW DATABASES LIKE '$name';" 2>$errorOutputPath`;
+
+      // Scan the error output for errors. If there are any, log them otherwise delete the log file
+      $errorCount = self::scanErrorOutput($errorOutputPath, $output);
+
+      if (false === $errorCount)
+      {
+        $output->writeln("<error>Error scanning $errorOutputPath file</error>");
         return false;
       }
 
-      $config = require $configFilename;
-
-      if (! isset($config['databases'][$name]))
+      if ($errorCount)
       {
-        $output->writeln('<error>Database does not exist.</error>');
+        $output->writeln("<error>Errors found! Check $errorOutputPath for more details.</error>");
         return false;
       }
 
-      return true;
+      if (false === unlink($errorOutputPath))
+      {
+        $output->writeln("<error>Error deleting $errorOutputPath file</error>");
+        return false;
+      }
+
+      $database = preg_grep("/$name/", explode("\n", $result));
+      return ! empty($database);
     }
     catch (Exception)
     {
@@ -113,19 +165,113 @@ class MySQLDatabase extends PDO implements DatabaseConnectionInterface
   }
 
   /**
+   * Scans the error output for errors.
+   *
+   * @param string $errorOutputPath The path to the error output file
+   * @param OutputInterface $output The output interface
+   * @return false|int
+   */
+  private static function scanErrorOutput(string $errorOutputPath, OutputInterface $output): false|int
+  {
+    $errorOutput = file($errorOutputPath);
+    $errorsFound = 0;
+
+    if (false === $errorOutput)
+    {
+      $output->writeln("<error>Error reading $errorOutputPath file</error>");
+      return false;
+    }
+
+    foreach ($errorOutput as $line)
+    {
+      $matchResult = preg_match('/ERROR/', $line);
+
+      if (false === $matchResult)
+      {
+        $output->writeln("<error>Error scanning $errorOutputPath file</error>");
+        return false;
+      }
+
+      if ($matchResult)
+      {
+        $output->writeln("<error>$line</error>");
+        $errorsFound++;
+      }
+    }
+
+    return $errorsFound;
+  }
+
+  /**
    * @inheritDoc
    */
-  public function setup(): int
+  public static function setup(?string $name = null): int
   {
-    $result = $this->query("CREATE DATABASE $this->name");
+    $input = new MockInput();
+    $output = new ConsoleOutput();
 
-    if (false === $result)
+    $type = DatabaseType::MYSQL->value;
+    $dbConfig = new DBConfig($input, $output, $name, $type);
+    if (Command::SUCCESS !== $dbConfig->load())
     {
-      $this->output->writeln('<error>Failed to create the database.</error>');
+      $output->writeln('<error>Failed to load database configuration.</error>');
       return Command::FAILURE;
     }
 
-    $this->output->writeln('<info>Database created.</info>');
+    $host = $dbConfig->get("$type.$name.host", DEFAULT_MYSQL_HOST);
+    $port = $dbConfig->get("$type.$name.port", DEFAULT_MYSQL_PORT);
+    $username = $dbConfig->get("$type.$name.username") ?? $dbConfig->get("$type.$name.user", DEFAULT_MYSQL_USER);
+    $password = $dbConfig->get("$type.$name.password") ?? '';
+
+    if (! self::exists($name))
+    {
+      $workingDirectory = getcwd() ?: '';
+      $errorOutputPath = Path::join($workingDirectory, time() . '.error.log');
+      $createResult = @`mysql -u $username -p$password -h $host -P $port -e "CREATE DATABASE $name;" 2>$errorOutputPath`;
+
+      if (false === $createResult)
+      {
+        $output->writeln('<error>Failed to create the database.</error>');
+        return Command::FAILURE;
+      }
+
+      $errorCount = self::scanErrorOutput($errorOutputPath, $output);
+
+      if (false === $errorCount)
+      {
+        $output->writeln("<error>Error scanning $errorOutputPath file</error>");
+        return Command::FAILURE;
+      }
+
+      if ($errorCount)
+      {
+        $output->writeln("<error>Errors found! Check $errorOutputPath for more details.</error>");
+        return Command::FAILURE;
+      }
+
+      if (false === unlink($errorOutputPath))
+      {
+        $output->writeln("<error>Error deleting $errorOutputPath file</error>");
+        return Command::FAILURE;
+      }
+    }
+
+    # Create the migrations table
+    $migrationsTableName = self::getMigrationsTableName();
+    $query = "CREATE TABLE IF NOT EXISTS $migrationsTableName (
+      migration VARCHAR(255) NOT NULL PRIMARY KEY,
+      ran_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+
+    $database = new self($name, $input, $output);
+
+    if (false === $database->exec($query) )
+    {
+      $output->writeln('<error>Failed to create the migrations table.</error>');
+      return Command::FAILURE;
+    }
+
+    $output->writeln("\n<info>MySQL database successfully set up.</info>", OutputInterface::VERBOSITY_VERBOSE);
     return Command::SUCCESS;
   }
 
@@ -144,5 +290,13 @@ class MySQLDatabase extends PDO implements DatabaseConnectionInterface
 
     $this->output->writeln('<info>Database dropped.</info>');
     return Command::SUCCESS;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function getMigrationsTableName(): string
+  {
+    return '__migrations';
   }
 }
