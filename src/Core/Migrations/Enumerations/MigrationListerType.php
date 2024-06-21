509986619914faf2faa99e8de938f533271fd8a8
Andrew Masiye <amasiye313@gmail.com>
2024-06-21 12:27:28 +0200

    docs(core/migrations/interfaces): add documenting comments to MigrationListerType.php

--- src/Core/Migrations/Enumerations/MigrationListerType.php
+++ src/Core/Migrations/Enumerations/MigrationListerType.php
@@ -2,9 +2,17 @@
 
 namespace Assegai\Console\Core\Migrations\Enumerations;
 
+/**
+ * Class MigrationListerType. This class provides the types of migration listers.
+ *
+ * @package Assegai\Console\Core\Migrations\Enumerations
+ */
 enum MigrationListerType: string
 {
+  // The type of migration lister that lists all migrations.
   case ALL = 'all';
+  // The type of migration lister that lists pending migrations.
   case PENDING = 'pending';
+  // The type of migration lister that lists ran migrations.
   case RAN = 'ran';
 }
