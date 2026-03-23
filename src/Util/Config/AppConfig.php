900ba791aa6783623f2ca1ed25b53fce52252755
Andrew Masiye <amasiye313@gmail.com>
2026-03-23 14:47:52 +0200

    feat: update output formatting for configuration update messages

--- src/Util/Config/AppConfig.php
+++ src/Util/Config/AppConfig.php
@@ -148,7 +148,7 @@ PHP
     }
 
     $filename = Path::join('config', basename($this->path));
-    $this->output->writeln("<question>UPDATE</question> $filename");
+    $this->output->writeln("<fg=bright-blue>UPDATE</> $filename");
     return Command::SUCCESS;
   }
 }
